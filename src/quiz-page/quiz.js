d2f7b48256db59a723d5e2493539276318a5057e
Ekalape <ekalape@outlook.it>
2022-11-07 13:42:07 +0100

    feat: implement mobile side menu and hamburger animation

--- src/quiz-page/quiz.js
+++ src/quiz-page/quiz.js
@@ -7,3 +7,13 @@ import '../assets/styles/footer.scss';
 import '../assets/icons/bird-logo-yellow.svg';
 import '../assets/icons/bird-logo-viol.svg';
 import '../assets/icons/rs_school_js.svg';
+
+const hamburger = document.querySelector('.hamburger');
+const menuMob = document.querySelector('.menu-mobile');
+
+hamburger.addEventListener('click', openMobileMenu);
+
+function openMobileMenu() {
+  hamburger.classList.toggle('open');
+  menuMob.classList.toggle('open');
+}
