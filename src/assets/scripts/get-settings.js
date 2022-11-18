e1aebae26e68dcbb3aab4faa144bf20376ca723f
Ekalape <ekalape@outlook.it>
2022-11-18 18:07:23 +0100

    fix: adjust z-indexes and header fixed position

--- src/assets/scripts/get-settings.js
+++ src/assets/scripts/get-settings.js
@@ -2,5 +2,5 @@ export default function getSettings() {
   if (localStorage.getItem('eklp_brdsqz_settings')) {
     console.log('getItem');
     return JSON.parse(localStorage.getItem('eklp_brdsqz_settings'));
-  } else return { language: 'ru', theme: 'dark' };
+  } else return { language: 'en', theme: 'dark' };
 }
